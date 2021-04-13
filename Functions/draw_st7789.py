79d65bc59694ee79c4041046d230732327c4b9a0
Birch Prat <birchprat@gmail.com>
2021-04-13 16:21:01 +0200

    Made changes in the main infodisplay code for migration to a 240x240 display -added vistemp function -added drawpic fucntion -some cleanup of files and folders

--- Functions/draw_st7789.py
+++ Functions/draw_st7789.py
@@ -17,7 +17,7 @@ def clearimage(disp, landscape='no'):
     draw.rectangle((0, 0, disp.width, disp.height), outline=0, fill=(0, 0, 0))
     disp.image(image, rotation)
     
-def displaywrite(disp, infos_dic, landscape='no'):
+def displaywrite(disp, infos_dic, landscape='no', rotation = 90):
     #x and y starting positions
     x = 0
     y = -2
@@ -29,7 +29,6 @@ def displaywrite(disp, infos_dic, landscape='no'):
         height = disp.height   # swap height/width to rotate to landscape
         width = disp.width
         
-    rotation = 90
     image = Image.new('RGB', (width, height))
     draw = ImageDraw.Draw(image)
 
@@ -40,5 +39,39 @@ def displaywrite(disp, infos_dic, landscape='no'):
 
     disp.image(image, rotation)
 
+def displaypic(disp, imageloc):
+    image = Image.open(f"{imageloc}")
+    
+    # Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = disp.width / disp.height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * disp.height // image.height
+        scaled_height = disp.height
+    else:
+        scaled_width = disp.width
+        scaled_height = image.height * disp.width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+    
+    # Crop and center the image
+    x = scaled_width // 2 - disp.width // 2
+    y = scaled_height // 2 - disp.height // 2
+    image = image.crop((x, y, x + disp.width, y + disp.height))
+    
+    # Display image.
+    disp.image(image)
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
--- Functions/gpio_settup.py
+++ Functions/gpio_settup.py
@@ -3,9 +3,9 @@ import adafruit_rgb_display.st7789 as st7789
 import board, digitalio
 from gpiozero import OutputDevice
 
-def displaysettup(init = 'yes', backlight = 'nothing'):
+def displaysettup(init = '240x240', backlight = 'nothing'):
     """Setting up the st7789 Display"""
-    if init == 'yes':
+    if init == '240x240':
         cs_pin = digitalio.DigitalInOut(board.CE0)
         dc_pin = digitalio.DigitalInOut(board.D25)
         reset_pin = None
@@ -13,11 +13,35 @@ def displaysettup(init = 'yes', backlight = 'nothing'):
         # Config for display baudrate (default max is 24mhz):
         BAUDRATE = 64000000
 
-        # Setup SPI bus using hardware SPI:
-        spi = board.SPI()
+        # Create the ST7789 display:
+        disp = st7789.ST7789(
+            board.SPI(),
+            cs=cs_pin,
+            dc=dc_pin,
+            rst=reset_pin,
+            baudrate=BAUDRATE,
+            height=240,
+            y_offset=80,
+            rotation=180
+        )
+
+        # Turn on the backlight
+        backlight = digitalio.DigitalInOut(board.D22)
+        backlight.switch_to_output()
+        backlight.value = True
+
+        return disp
+    
+    if init == '240x135':
+        cs_pin = digitalio.DigitalInOut(board.CE0)
+        dc_pin = digitalio.DigitalInOut(board.D25)
+        reset_pin = None
+
+        # Config for display baudrate (default max is 24mhz):
+        BAUDRATE = 64000000
 
         # Create the ST7789 display:
-        disp = st7789.ST7789(spi, cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE,
+        disp = st7789.ST7789(board.SPI(), cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE,
                              width=135, height=240, x_offset=53, y_offset=40)
 
         # Turn on the backlight
